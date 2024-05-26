5cff596f9c841c189e2cdab83f0ba6ecf8cdb0da
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-05-26 20:07:04 +0600

    update crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'product_list_screen.dart';
+import 'package:practiceapp/app.dart';
+
 void main() {
   runApp(const CrudApp());
-}
-
-class CrudApp extends StatelessWidget {
-  const CrudApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: ProductListScreen()
-    );
-
-  }
 }
