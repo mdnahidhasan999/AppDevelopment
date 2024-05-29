c59113d63c44877d49dc3c0b0f1d1536f8cafc06
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-05-29 23:42:08 +0600

    update crud

--- lib/product_list_screen.dart
+++ lib/product_list_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:practiceapp/add_product.dart';
 import 'package:practiceapp/update_product_screen.dart';
 
@@ -10,18 +14,34 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  bool _getProductListInProgress = false;
+  List<Product> productList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Product list'),
       ),
-      body: ListView.separated(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return _buildProductItem();
-        },
-        separatorBuilder: (_, __) => const Divider(),
+      body: Visibility(
+        visible: _getProductListInProgress == false,
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+        child: ListView.separated(
+          itemCount: productList.length,
+          itemBuilder: (context, index) {
+            return _buildProductItem(productList[1]);
+          },
+          separatorBuilder: (_, __) => const Divider(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -35,16 +55,55 @@ class _ProductListScreenState extends State<ProductListScreen> {
     );
   }
 
-  Widget _buildProductItem() {
+  Future<void> _getProductList() async {
+    _getProductListInProgress = true;
+    setState(() {});
+    productList.clear();
+    const String getProductListUrl =
+        'https://crud.teamrabbil.com/api/v1/ReadProduct';
+    Uri uri = Uri.parse(getProductListUrl);
+
+    Response response = await get(uri);
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      final decodedData = jsonDecode(response.body);
+
+      final jsonproductList = decodedData['date'];
+
+      for (Map<String, dynamic> p in jsonproductList) {
+        Product product = Product(
+          id: p['_id'] ?? '',
+          productName: p['ProductName'] ?? '',
+          productCode: p['ProductCode'] ?? '',
+          image: p['Img'] ?? '',
+          unitPrice: p['UnitPrice'] ?? '',
+          quantity: p['QTY'] ?? '',
+          totalPrice: p['TotalPrice'] ?? '',
+        );
+        productList.add(product);
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text(' Get Product List Failed!')));
+    }
+    _getProductListInProgress = false;
+    setState(() {
+
+    });
+  }
+
+  Widget _buildProductItem(Product product) {
     return ListTile(
-      leading: const CircleAvatar(backgroundImage: AssetImage('assets/images/profile_picture.jpg')),
-      title: const Text('Product name'),
-      subtitle: const Wrap(
+      leading: const CircleAvatar(
+          backgroundImage: AssetImage('assets/images/profile_picture.jpg')),
+      title:  Text(product.productName),
+      subtitle:  Wrap(
         spacing: 16,
         children: [
-          Text('Unit Price: 100'),
-          Text('Quantity : 100'),
-          Text('Total Price: 10000'),
+          Text('Unit Price: ${product.unitPrice}'),
+          Text('Quantity : ${product.quantity}'),
+          Text('Total Price: ${product.totalPrice}'),
         ],
       ),
       trailing: Wrap(
@@ -76,7 +135,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
       builder: (context) {
         return AlertDialog(
           title: const Text('Delete'),
-          content: const Text('Are you sure that you want to delete this product?'),
+          content:
+          const Text('Are you sure that you want to delete this product?'),
           actions: [
             TextButton(
               onPressed: () {
@@ -95,4 +155,32 @@ class _ProductListScreenState extends State<ProductListScreen> {
       },
     );
   }
+}
+
+class Product {
+  // 'id' : "",
+  // 'ProductName' : "",
+  // 'ProductCode' : "",
+  // 'Img' : "",
+  // 'UnitPrice' : "210",
+  // 'Qty' : "",
+  // 'TotalPrice' : "",
+  // 'CreatedDate' : "",
+  final String id;
+  final String productName;
+  final String productCode;
+  final String image;
+  final String unitPrice;
+  final String quantity;
+  final String totalPrice;
+
+  Product({
+    required this.id,
+    required this.productName,
+    required this.productCode,
+    required this.image,
+    required this.unitPrice,
+    required this.quantity,
+    required this.totalPrice,
+  });
 }
