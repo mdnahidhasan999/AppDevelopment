bc3f107ad53f95c43f0c18ad07f57a1939356bb7
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-06-03 11:18:35 +0600

    Class Rabbil

--- lib/Screen/ProductCreateScreen.dart
+++ lib/Screen/ProductCreateScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Style/style.dart';
+
 class ProductCreateScreen extends StatefulWidget {
   const ProductCreateScreen({super.key});
 
@@ -16,18 +18,88 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
       ),
       body: Stack(
         children: [
+          ScreenBackground(context),
           Container(
             child: (SingleChildScrollView(
-             padding: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               child: Column(
                 children: [
-                  TextField(onChanged: (value){},)
+                  TextField(
+                    onChanged: (value) {},
+                    decoration: AppInputDecoration('Product Name'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    onChanged: (value) {},
+                    decoration: AppInputDecoration('Product Code'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    onChanged: (value) {},
+                    decoration: AppInputDecoration('Product Image'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    onChanged: (value) {},
+                    decoration: AppInputDecoration('Unit Price'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    onChanged: (value) {},
+                    decoration: AppInputDecoration('Total Price'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  AppDropDownStyle(
+                    DropdownButton(
+                      value: "",
+                      items: const [
+                        DropdownMenuItem(
+                          value: '',
+                          child: Text('Select Qt'),
+                        ),
+                        DropdownMenuItem(
+                          value: '1 pcs',
+                          child: Text('1 pcs'),
+                        ),
+                        DropdownMenuItem(
+                          value: '2 pcs',
+                          child: Text('2 pcs'),
+                        ),
+                        DropdownMenuItem(
+                          value: '3 pcs',
+                          child: Text('3 pcs'),
+                        ),
+                        DropdownMenuItem(
+                          value: '4 pcs',
+                          child: Text('4 pcs'),
+                        ),
+                      ],
+                      onChanged: (value) {},
+                      underline: Container(),
+                      isExpanded: true,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    child: ElevatedButton(
+                        style: AppButtonStyle(),
+                        onPressed: () {},
+                        child: SuccessButtonChild('Submit')),
+                  )
                 ],
-
               ),
             )),
           )
-
         ],
       ),
     );
