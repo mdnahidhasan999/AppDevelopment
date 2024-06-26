44bc0a4d229f9df506f84c5b34054c9b587d36c3
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-06-26 09:59:29 +0600

    counter app rabbil

--- lib/main.dart
+++ lib/main.dart
@@ -4,161 +4,47 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-
-        theme: ThemeData(primarySwatch: Colors.green),
-        darkTheme: ThemeData(primarySwatch: Colors.blue),
-        color: Colors.black,
-        debugShowCheckedModeBanner: false,
-        home:const HomeActivity()
-
+    return const MaterialApp(
+      title: "Counter App",
+      home: MyHomePage(),
     );
   }
-
-
-
 }
 
-class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  MySnackBar(message,context) async => ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text(message)));
-
+class _MyHomePageState extends State<MyHomePage> {
+  int countNumber = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       appBar: AppBar(
-
-        title: const Text("Inventory App"),
-        titleSpacing: 15,
-        //centerTitle: true,
-        toolbarHeight: 60,
-        toolbarOpacity: 1,
-        elevation: 10,
-        backgroundColor: Colors.blueAccent,
-        actions: [
-          IconButton(onPressed: (){MySnackBar("Comments", context);}, icon: const Icon(Icons.comment)),
-          IconButton(onPressed: (){MySnackBar("Search", context);}, icon: const Icon(Icons.search))
-        ],
-
+        title: const Text('Counter App'),
       ),
-
+      body: Center(
+        child: Text(countNumber.toString()),
+      ),
       floatingActionButton: FloatingActionButton(
-
-        elevation: 10,
         child: const Icon(Icons.add),
-        backgroundColor: Colors.amber,
-        onPressed: (){
-          MySnackBar("I am floating action button",context);
-
+        onPressed: () {
+          setState(
+            () {
+              countNumber = countNumber + 1;
+            },
+          );
         },
-
-
       ),
-
-
-      bottomNavigationBar: BottomNavigationBar(
-
-        //currentIndex: 1,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.contacts), label: "Contact"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile" )
-
-        ],
-
-        onTap: (int index){
-          if(index==0){
-
-            MySnackBar("I am home Menu", context);
-
-          }if(index==1){
-            MySnackBar("I am contacts Menu", context);
-          }if(index==2){
-            MySnackBar("I am Profile Menu", context);
-          }
-
-        },
-
-
-      ),
-
-      drawer: Drawer(
-
-        child: ListView(
-
-          children: [
-            DrawerHeader(
-
-                padding: const EdgeInsets.all(0),
-                child: UserAccountsDrawerHeader(
-                  decoration: const BoxDecoration(color: Colors.black),
-                  accountName:
-                  const Text("Nahid Hasan",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 20,
-                      //fontWeight:FontWeight.w600,
-                      backgroundColor: Colors.redAccent,
-
-
-
-                    ),),
-                  accountEmail: const Text("Info@Nahid.com"),
-                  currentAccountPicture: Image.network("https://www.google.com/search?client=firefox-b-d&sca_esv=ea05bd65b2dab007&sca_upv=1&q=%40mdnahidhasan999&uds=AMwkrPu_SJTRB4ZX2y7zhHd8IFRv9oTxj2rfp0oLvt4zevAjgIzxBynz_33SKEsIl7HH5kqIYOKmnXsZYSmLg8x7gYv9pD48ivVruhoEimNrUIAI9j-U9Q-K8ASJqVZkq0pvElww8FwdIvNFUwdTEXKac3OfHv7HslraEU5M-10YxGYNfhsmsF1scUU7vUnJkKuasSVR2EQ_I3WSGWKBfiM76oc5LTwGIIkAjanEDLGZURG3GklOb9n6PAm_Yl50yUAiwTaERN5InnATsSFzAo6dBOK2uJbXBfa5YBNr45_cfGJq3wsxycc&udm=2&prmd=ivsnmbz&sa=X&ved=2ahUKEwiX6dfkp8OFAxUmzzgGHcd1AqcQtKgLegQIDRAB&biw=1366&bih=635&dpr=1#vhid=yqiRq-Xho_McEM&vssid=mosaic"),
-
-                )
-
-            ),
-            ListTile(
-                title: const Text("Home"),
-                leading: const Icon(Icons.home),
-                onTap:(){
-                  MySnackBar("Home", context);
-                }),
-            ListTile(
-                title: const Text("Contact"),
-                leading: const Icon(Icons.contacts),
-                onTap:(){
-                  MySnackBar("Contact", context);
-                }),
-            ListTile(
-                title: const Text("Profile"),
-                leading: const Icon(Icons.person),
-                onTap:(){
-                  MySnackBar("Profile", context);
-                }),
-            ListTile(
-                title: const Text("Email"),
-                leading: const Icon(Icons.email),
-                onTap:(){
-                  MySnackBar("Email", context);
-                }),
-            ListTile(
-                title: const Text("Phone"),
-                leading: const Icon(Icons.phone),
-                onTap:(){
-                  MySnackBar("Phone", context);
-                })
-
-          ],
-        ),
-
-      ),
-
     );
-
   }
-
 }
-
